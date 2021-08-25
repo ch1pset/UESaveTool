3e4f5609e91c541fe649137f44f6561f14bbd66c
Josh <joshneilgonzales@gmail.com>
2021-08-25 15:41:15 -0500

    removed uuid import reference

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 import * as fs from 'fs';
 import { Buffer } from 'buffer';
 import { argv } from 'process';
-import * as uuid from 'uuid';
 import { Reader } from './IOStream.js'
-export { fs, Buffer, argv, uuid, Reader }
+export { fs, Buffer, argv, Reader }
