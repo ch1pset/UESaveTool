1c2307ce1849fa10a017899f408fdd910209ceb2
Josh <joshneilgonzales@gmail.com>
2021-08-25 16:13:21 -0500

    Added Writer to imports

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 import { Buffer } from 'buffer';
 import { argv } from 'process';
-import { Reader } from './IOStream.js'
-export { fs, Buffer, argv, Reader }
+import { Reader, Writer } from './IOStream.js'
+export { fs, Buffer, argv, Reader, Writer }
