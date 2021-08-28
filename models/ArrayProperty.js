eaa8a53ff2ed82b074c189a864c9c12a2d4501e4
Josh <joshneilgonzales@gmail.com>
2021-08-28 02:01:42 -0500

    begin `ArrayProperty` rework

--- models/ArrayProperty.js
+++ models/ArrayProperty.js
@@ -11,75 +11,71 @@ import {
 } from './index.js'
 
 export class ArrayProperty extends Property {
-    constructor(name, type, value, atype, aname, ptype, pname) {
+    constructor(name, type, value, stype) {
         super(name, type, value);
-        this.ArrayType = atype;
-        this.ArrayName = aname;
-        this.ArrayPropertyType = ptype;
-        this.ArrayPropertyName = pname;
+        this.StoredPropertyType = stype;
+        // this.ArrayName = aname;
+        // this.ArrayPropertyType = ptype;
+        // this.ArrayPropertyName = pname;
         // this.Array = arr;
         // this.StoredSize = length;
         // console.log(`Generated Array Size: ${this.Size}`);
     }
     get Size() {
-        let size = 0;
-        size += 4;
-        size += this.ArrayName.length + 4;
-        size += this.ArrayPropertyType.length + 4;
-        size += 4;
-        size += 4;
-        size += this.ArrayPropertyName.length + 4;
-        size += 17;
-        for(let i = 0; i < this.Value.length; i++) {
-            for(let j = 0; j < this.Value[i].length; j++) {
-                size += this.Value[i][j].Size;
-            }
-            size += 9;
-        }
-        return size;
+        // let size = 0;
+        // size += 4;
+        // size += this.ArrayName.length + 4;
+        // size += this.ArrayPropertyType.length + 4;
+        // size += 4;
+        // size += 4;
+        // size += this.ArrayPropertyName.length + 4;
+        // size += 17;
+        // for(let i = 0; i < this.Value.length; i++) {
+        //     for(let j = 0; j < this.Value[i].length; j++) {
+        //         size += this.Value[i][j].Size;
+        //     }
+        //     size += 9;
+        // }
+        // return size;
     }
     static from(obj) {
         let array = new ArrayProperty();
         array.Name = obj.Name;
         array.Type = obj.Type;
-        array.ArrayType = obj.ArrayType;
-        array.ArrayName = obj.ArrayName;
-        array.ArrayPropertyType = obj.ArrayPropertyType;
-        array.ArrayPropertyName = obj.ArrayPropertyName;
+        array.StoredPropertyType = obj.StoredPropertyType;
+        // array.ArrayName = obj.ArrayName;
+        // array.ArrayPropertyType = obj.ArrayPropertyType;
+        // array.ArrayPropertyName = obj.ArrayPropertyName;
         // array.StoredSize = obj.StoredSize;
         array.Value = [];
-        obj.Value.forEach((arr) => {
-            let pair = [];
-            arr.forEach((prop) => {
-                switch(prop.Type)
-                {
-                    case 'BoolProperty\0':
-                        pair.push(BoolProperty.from(prop));
-                        break;
-                    case 'IntProperty\0':
-                        pair.push(IntProperty.from(prop));
-                        break;
-                    case 'FloatProperty\0':
-                        pair.push(FloatProperty.from(prop));
-                        break;
-                    case 'StrProperty\0':
-                        pair.push(StrProperty.from(prop));
-                        break;
-                    case 'ObjectProperty\0':
-                        pair.push(ObjectProperty.from(prop));
-                        break;
-                    case 'SoftObjectProperty\0':
-                        pair.push(SoftObjectProperty.from(prop));
-                        break;
-                    case 'EnumProperty\0':
-                        pair.push(EnumProperty.from(prop));
-                        break;
-                    default:
-                        throw new Error(`Unrecognized Property: ${prop.Type}`);
-                }
-            })
-            array.Value.push(pair);
-        })
+        // obj.Value.forEach((prop) => {
+        //     switch(prop.Type)
+        //     {
+        //         case 'BoolProperty\0':
+        //             array.push(BoolProperty.from(prop));
+        //             break;
+        //         case 'IntProperty\0':
+        //             array.push(IntProperty.from(prop));
+        //             break;
+        //         case 'FloatProperty\0':
+        //             array.push(FloatProperty.from(prop));
+        //             break;
+        //         case 'StrProperty\0':
+        //             array.push(StrProperty.from(prop));
+        //             break;
+        //         case 'ObjectProperty\0':
+        //             array.push(ObjectProperty.from(prop));
+        //             break;
+        //         case 'SoftObjectProperty\0':
+        //             array.push(SoftObjectProperty.from(prop));
+        //             break;
+        //         case 'EnumProperty\0':
+        //             array.push(EnumProperty.from(prop));
+        //             break;
+        //         default:
+        //             throw new Error(`Unrecognized Property: ${prop.Type}`);
+        //     }
+        // })
         return array;
     }
 }
--- utils/Reader.js
+++ utils/Reader.js
@@ -88,76 +88,111 @@ export class Reader extends FileIO {
 
     readProperty(name) {
         let type = this.readString();
-        let value, length;
+        let start = this.tell;
+        let length = this.readInt32();
+        let value;
+        // console.log(`Name: ${name} Type: ${type} Size: ${length}`)
         switch(type)
         {
             case 'BoolProperty\0':
-                this.seek(8);
+                this.seek(4);
                 value = this.readByte() === 0;
                 this.seek(1);
                 return new BoolProperty(name, type, value);
+
             case 'IntProperty\0':
-                length = this.readInt32();
                 let int1 = this.readInt32(); // floor
                 this.seek(1);
                 let int2 = this.readInt32();
                 value = [int1, int2]
                 return new IntProperty(name, type, value);
+
             case 'FloatProperty\0':
-                this.seek(9);
+                this.seek(5);
                 value = this.readFloat();
                 return new FloatProperty(name, type, value);
+
             case 'StrProperty\0':
-                length = this.readInt32();
                 this.seek(5);
                 value = this.readString();
                 return new StrProperty(name, type, value);
+
             case 'ObjectProperty\0':
-                length = this.readInt32();
                 this.seek(5);
                 value = this.readString();
                 return new ObjectProperty(name, type, value);
+
             case 'SoftObjectProperty\0':
-                length = this.readInt32();
                 this.seek(5);
                 value = this.readString();
                 this.seek(4);
                 return new SoftObjectProperty(name, type, value);
+
             case 'StructProperty\0':
-                length = this.readInt32();
                 this.seek(4);
                 let stype = this.readString();
                 this.seek(17);
                 let props = this.readProperties();
                 return new StructProperty(name, type, value, stype, props)
+
             case 'ArrayProperty\0':
-                let start = this.tell;
-                length = this.readInt32(); // stored struct size in bytes
                 this.seek(4);
                 let atype = this.readString();
                 this.seek(1);
-                // let start2 = this.tell;
                 let alength = this.readInt16();
                 this.seek(2);
-                let aname = this.readString();
-                let ptype = this.readString();
-                let toEnd = this.readInt32(); //length - ((this.tell - start2) + pname.length + 29)
-                this.seek(4);
-                let pname = this.readString();
-                this.seek(17);
-                value = [];
-                while(this.tell < (start + length)) {
-                    let propItem = this.readProperties();
-                    value.push(propItem);
+                let i = 0;
+                switch(atype)
+                {
+                    case 'IntProperty\0':
+                        this.seek(4);
+                        let int = {};
+                        int.Name = this.readString();
+                        int.Type = this.readString();
+                        this.readInt32();
+                        int.Value = []
+                        while(i < alength) {
+                            this.seek(5);
+                            int.Value.push(this.readInt32())
+                            i++;
+                        }
+                        value = IntProperty.from(int);
+                        break;
+                    case 'SoftObjectProperty\0':
+                        value = [];
+                        while(i < alength) {
+                            value.push(this.readString());
+                            this.seek(4);
+                            i++;
+                        }
+                        break;
+                    case 'StructProperty\0':
+                        let struct = {};
+                        struct.Name = this.readString();
+                        struct.Type = this.readString();
+                        struct.size = this.readInt32();
+                        this.seek(4);
+                        struct.StoredPropertyType = this.readString();
+                        this.seek(17);
+                        struct.Properties = []
+                        while(i < alength) {
+                            struct.Properties.push(this.readProperties());
+                            i++;
+                        }
+                        console.log(struct);
+                        value = struct;
+                        break;
                 }
-                return new ArrayProperty(name, type, value, atype, aname, ptype, pname);
+                // console.log(value);
+                return new ArrayProperty(name, type, value, atype);
+
             case 'EnumProperty\0':
-                length = this.readInt32();
                 this.seek(4);
                 let etype = this.readString(); //same as type
                 this.seek(1);
                 value = this.readString();
                 return new EnumProperty(name, type, value, etype);
+
             default:
                 throw new Error(`Unrecognized Property: ${type}`);
         }
