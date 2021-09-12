414a034beb18790a4ef1d0423cc5e904a793d678
Josh <joshneilgonzales@gmail.com>
2021-09-12 02:49:39 -0500

    Make `PropertyFactory` a const for compatability

--- models/factories/PropertyFactory.js
+++ models/factories/PropertyFactory.js
@@ -1,20 +1,20 @@
 import { TypeNotImplementedError } from '../index.js';
 
-export class PropertyFactory {
-    static Properties = {};
-    static Arrays = {};
-    static add(ctor) {
+export const PropertyFactory = {
+    Properties: {},
+    Arrays:{},
+    add: function(ctor) {
         this.Properties[ctor.name] = ctor;
-    }
-    static create(obj) {
+    },
+    create: function(obj) {
         let type = obj.Type.split('\0')[0]
 
         if (this.Properties[type] === undefined)
             throw new TypeNotImplementedError(type);
 
         return this.Properties[type].from(obj);
-    }
-    static createArray(obj) {
+    },
+    createArray: function(obj) {
         let type = obj.Type.split('\0')[0]
 
         if (this.Arrays[type] === undefined)
